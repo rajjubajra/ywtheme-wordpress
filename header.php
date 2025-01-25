13439606bce73a72353bf915b23666e4163476a9
rajjubajra <rajjubajra@gmail.com>
2025-01-25 06:14:42 +0000

    theme file updated

--- header.php
+++ header.php
@@ -11,6 +11,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WordPress Theme</title>
-    <?php wp_head(); ?>
+    <?php wp_header(); ?>
 </head>
 <body>
