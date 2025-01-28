9423bdfd75abdd5220ad53daad7010b4f2feac4b
rajjubajra <rajjubajra@gmail.com>
2025-01-28 08:54:25 +0000

    new tailwindcss theme installed

--- footer.php
+++ footer.php
@@ -1,12 +1,7 @@
-<?php
-/** 
- * Footer template for our theme
- * 
- * @package ywtheme-wordpress
- */
-?>
+<div class="border-t mt-4 py-4">
+  <div class="max-w-4xl px-4 mx-auto text-xs text-gray-400 text-center">Copyright &copy; <?php echo date("Y"); ?> Fictional Company</div>
+</div>
 
-<footer>Footer section ???</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
